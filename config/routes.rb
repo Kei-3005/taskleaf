aa41271e9383e22d708f2fbdeb04df3bed986773
kei-3005 <mindovermatter3005@gmail.com>
2022-07-13 22:03:48 +0900

    chapter7-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   namespace :admin do
-    namespace :admin do
-      resources :users
-    end
+    resources :users
   end
   
   root to: 'tasks#index'
